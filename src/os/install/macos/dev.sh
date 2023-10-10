dadf9c07eafe44b03172fd4c2e22760412b7c850
Gabriel Freiberg <gabefreiberg@gmail.com>
2023-10-09 19:29:39 -0600

    Adds docker

--- src/os/install/macos/dev.sh
+++ src/os/install/macos/dev.sh
@@ -9,8 +9,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 print_in_purple "\n   Compression Tools\n\n"
 
 brew_install "AWS CLI" "awscli"
+brew_install "Cyberduck" "cyberduck" "--cask"
+brew_install "Docker" "docker" "--cask"
 brew_install "Postman" "postman" "--cask"
 brew_install "Soap UI" "soapui" "--cask"
 brew_install "Source Tree" "sourcetree" "--cask"
-brew_install "Cyberduck" "cyberduck" "--cask"
 brew_install "Virtual Box" "virtualbox" "--cask"
