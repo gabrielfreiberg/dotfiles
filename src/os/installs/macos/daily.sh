8927db7c47888a9f3c4004f5914164f9e133bd18
Gabe Freiberg <gabefreiberg@gmail.com>
2024-04-03 14:35:00 -0600

    Fix alfred to v4; bitwarden-cli is not a cask

--- src/os/installs/macos/daily.sh
+++ src/os/installs/macos/daily.sh
@@ -9,12 +9,12 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 print_in_purple "\n   Daily Tools\n\n"
 
 brew_install "Adobe Acrobat Reader" "adobe-acrobat-reader" "--cask"
-brew_install "Alfred" "alfred" "--cask"
+brew_install "Alfred" "homebrew/cask-versions/alfred4" "--cask"
 brew_install "Bartender" "bartender" "--cask"
 brew_install "Google Drive" "google-drive" "--cask"
 brew_install "insync" "insync" "--cask" # configure me
 brew_install "bitwarden" "bitwarden" "--cask"
-brew_install "bitwarden cli" "bitwarden-cli" "--cask"
+brew_install "bitwarden cli" "bitwarden-cli"
 brew_install "Obsidian" "obsidian" "--cask"
 brew_install "Sizeup" "sizeup" "--cask"
 brew_install "Slack" "slack" "--cask"
