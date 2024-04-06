91f3ecf4b686f36ed6605c2260d49d594d3e8e8a
Gabe Freiberg <gabefreiberg@gmail.com>
2024-04-06 14:12:06 -0600

    Adds blueutil to dotfiles to allow for trackpad script

--- src/os/installs/macos/misc.sh
+++ src/os/installs/macos/misc.sh
@@ -9,6 +9,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 print_in_purple "\n   Miscellaneous\n\n"
 
 # brew_install "Calibre E-Book" "calibre" "--cask"
+brew_install "Bluetooth Utility" "blueutil"
 brew_install "Cheat Sheet" "cheatsheet" "--cask"
 brew_install "Handbrake" "handbrake" "--cask"
 brew_install "Proton VPN" "protonvpn" "--cask"
